eea34469ca4ed5d415900785373209e3a60e1896
Martin Flodin <mflodin@gmail.com>
2011-10-22 14:11:55 +0200

    Added some more tests

--- src/se/mflodin/yankeeconverter/Converter.java
+++ src/se/mflodin/yankeeconverter/Converter.java
@@ -3,13 +3,35 @@ package se.mflodin.yankeeconverter;
 public class Converter {
 	final float FAHRENHEIT_OFFSET = 32.0f;
 	final float FAHRENHEIT_FACTOR = 9.0f/5.0f;
+	final float POUND_FACTOR = 0.45359237f;
+	
+//	public final enum UNITS {
+//		
+//		
+//	}
 
 	public Converter() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public float fahrenheitToCelcius(float degrees){
+		// Tc = (5/9)*(Tf-32)
+		return (degrees - FAHRENHEIT_OFFSET) / FAHRENHEIT_FACTOR;
+	}
+
+	public float celciusToFahrenheit(float degrees) {
+		// Tf = (9/5)*Tc+32
 		return degrees * FAHRENHEIT_FACTOR + FAHRENHEIT_OFFSET;
 	}
+
+	public float poundsToKilograms(float pounds) {
+		return pounds * POUND_FACTOR;
+	}
+	
+	public float kilogramsToPounds(float kilograms) {
+		return kilograms / POUND_FACTOR;
+	}
+	
+	//private float fromSI(float value, )
 
 }
--- tests/src/se/mflodin/yankeeconverter/test/ConversionTest.java
+++ tests/src/se/mflodin/yankeeconverter/test/ConversionTest.java
@@ -18,13 +18,43 @@ public class ConversionTest extends TestCase {
 		converter = new Converter();
 	}
 	
-public void testConvertFromFahrenheitToCelsius(){
-		float fahrenheit = 0.0f;
-		float expectedCelsius = 32.0f;
+	public void testConvertFromFahrenheitToCelsius(){
+		float fahrenheit = 32.0f;
+		float expectedCelsius = 0.0f;
 		float celsius = converter.fahrenheitToCelcius(fahrenheit);
-		assertTrue(celsius == expectedCelsius);
+		assertEquals(expectedCelsius, celsius);
 		
+		fahrenheit = 212.0f;
+		expectedCelsius = 100.0f;
+		celsius = converter.fahrenheitToCelcius(fahrenheit);
+		assertEquals(expectedCelsius, celsius);
 	}
 
+	public void testConvertFromCelsiusToFahrenheit(){
+		float celsius = 100.0f;
+		float expectedFahrenheit = 212.0f;
+		float fahrenheit = converter.celciusToFahrenheit(celsius);
+		assertEquals(expectedFahrenheit, fahrenheit);
+		
+		celsius = 0.0f;
+		expectedFahrenheit = 32.0f;
+		fahrenheit = converter.celciusToFahrenheit(celsius);
+		assertEquals(expectedFahrenheit, fahrenheit);
+	}
+	
+	public void testConvertFromPoundsToKilograms(){
+		float pounds = 1.0f;
+		float expectedKilograms = 0.45359237f;
+		float kilograms = converter.poundsToKilograms(pounds);
+		assertEquals(expectedKilograms, kilograms);
+	}
+	
+	public void testConvertFromKilogramsToPounds(){
+		float kilograms = 1.0f;
+		float expectedPounds = 1 / 0.45359237f;
+		float pounds = converter.kilogramsToPounds(kilograms);
+		assertEquals(expectedPounds, pounds);
+	}
+	
 
 }
